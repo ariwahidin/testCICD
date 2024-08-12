03090232b12eb32f437f1d73216b800de82cd4d1
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-08-12 09:45:39 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+// Use the PORT from .env or default to 3000
+const PORT = process.env.PORT || 3100;
+
+// Route untuk halaman utama
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Mulai server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
